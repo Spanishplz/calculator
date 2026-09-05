eb5a6476d234853d1290647c42a32c6c551cd42a
spanishplz <spanishplz@gmail.com>
2026-09-05 15:22:51 -0500

    Add basic layout of the calculator

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
+// first buttons
+let numbers = [ 0, ".", "=", 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let operations = ["+", "-", "x", "÷", "clear"].reverse();
+
+const calculatorResults = document.querySelector("#calculatorResults");
+const numbersDiv = document.querySelector("#numbers");
+const symbolsDiv = document.querySelector("#symbols");
+
+// create screen
+const screen = document.createElement("input");
+screen.setAttribute("id", "screen");
+calculatorResults.appendChild(screen);
+
+// create number buttons
+numbers.forEach((elem) => {
+    const button = document.createElement("div");
+    button.classList.add("button");
+    numbersDiv.appendChild(button);
+    button.textContent = elem.toString();
+    button.setAttribute("value", `${elem}`);
+
+});
+
+// create symbols buttons
+operations.forEach((elem) => {
+    const symButton = document.createElement("div");
+    symButton.classList.add("button");
+    symButton.classList.add("symbols");
+    symButton.textContent = elem.toString();
+    symButton.setAttribute("value", `${elem}`);
+    symbolsDiv.appendChild(symButton);
+});
+
+
+
 
